71331884dfc120c5f3b9bf6dc2c25834a8b95422
Ashish Kumar <ashishkumar60719@gmail.com>
2026-05-05 15:44:07 +0530

    Update CORS origin to production URL

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://major-1-wr6v.onrender.com",
     credentials: true
 }))
 

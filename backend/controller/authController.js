739bae15c4d88e8d490d7a9d46b916940cfe209f
Ashish Kumar <ashishkumar60719@gmail.com>
2026-05-05 17:28:45 +0530

    Update authController.js

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -51,7 +51,7 @@ export const login = async(req,res) => {
         let token = await genToken(user._id)
         res.cookie("token",token, {
             httpOnly:true,
-            secure:ture,
+            secure:true,
             sameSite:"none",
             maxAge:7*24*60*60*1000
         })
